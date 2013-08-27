714f32e72cd5d4d72d2b9b5d6699717f5979b9fe
TheWonderWall <thewonderwall@meneame.net>
2013-08-27 11:42:52 +0300

    Text selection and link click support.

--- src/net/meneame/fisgodroid/ChatLineView.java
+++ src/net/meneame/fisgodroid/ChatLineView.java
@@ -1,13 +1,19 @@
 package net.meneame.fisgodroid;
 
-import org.xml.sax.XMLReader;
+import java.util.Locale;
 
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.text.Editable;
+import android.os.Build;
 import android.text.Html;
+import android.text.Selection;
+import android.text.Spannable;
 import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -19,9 +25,45 @@ public class ChatLineView extends LinearLayout
     private TextView mUsername;
     private TextView mMessage;
     private ImageView mAvatar;
-    
+
+    // From
+    // http://stackoverflow.com/questions/15836306/can-a-textview-be-selectable-and-contain-links
+    // Thanks oakleaf!
+    private static class CustomMovementMethod extends LinkMovementMethod
+    {
+        @Override
+        public boolean canSelectArbitrarily()
+        {
+            return true;
+        }
+
+        @Override
+        public void initialize(TextView widget, Spannable text)
+        {
+            Selection.setSelection(text, text.length());
+        }
+
+        @Override
+        public void onTakeFocus(TextView view, Spannable text, int dir)
+        {
+            if ( (dir & (View.FOCUS_FORWARD | View.FOCUS_DOWN)) != 0 )
+            {
+                if ( view.getLayout() == null )
+                {
+                    // This shouldn't be null, but do something sensible if it
+                    // is.
+                    Selection.setSelection(text, text.length());
+                }
+            }
+            else
+            {
+                Selection.setSelection(text, text.length());
+            }
+        }
+    }
+
     // Runable to asynchronously set the avatar image
-    private Runnable mSetAvatarRunnable = new Runnable ()
+    private Runnable mSetAvatarRunnable = new Runnable()
     {
         @Override
         public void run()
@@ -34,37 +76,50 @@ public class ChatLineView extends LinearLayout
             }
         }
     };
-    
+
     public ChatLineView(Context context, AvatarStorage avatarStorage)
     {
         super(context);
         mAvatarStorage = avatarStorage;
-        
+
         // Create the view
         LayoutInflater.from(getContext()).inflate(R.layout.chat_line, this, true);
-        mMessage = (TextView)findViewById(R.id.chat_message);
-        mUsername = (TextView)findViewById(R.id.chat_username);
-        mAvatar = (ImageView)findViewById(R.id.chat_avatar);
+        mMessage = (TextView) findViewById(R.id.chat_message);
+        mUsername = (TextView) findViewById(R.id.chat_username);
+        mAvatar = (ImageView) findViewById(R.id.chat_avatar);
+
+        setSelectableText();
+        mMessage.setMovementMethod(new CustomMovementMethod());
     }
 
-    public void setChatMessage ( String username, ChatMessage chatMsg )
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private void setSelectableText()
+    {
+        // Set the text selectable on API level >= 11
+        if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB )
+        {
+            mMessage.setTextIsSelectable(true);
+        }
+    }
+
+    public void setChatMessage(String username, ChatMessage chatMsg)
     {
         mChatMsg = chatMsg;
         if ( mChatMsg != null )
         {
             String parsedMessage = Smileys.parseMessage(mChatMsg.getMessage());
-            
+
             // Highlight when they mention our name
             if ( parsedMessage.toLowerCase().contains(username.toLowerCase()) )
             {
                 parsedMessage = "<b>" + parsedMessage + "</b>";
             }
-            
+
             Spanned spannedMessage = Html.fromHtml(parsedMessage, Smileys.getImageGetter(getContext()), null);
             mMessage.setText(spannedMessage);
             mUsername.setText(mChatMsg.getUser());
             mAvatarStorage.request(mChatMsg.getIcon(), mSetAvatarRunnable);
-            
+
             if ( mChatMsg.getType() == ChatType.PUBLIC )
                 mMessage.setTextColor(getResources().getColor(R.color.text_chat_general));
             else if ( mChatMsg.getType() == ChatType.FRIENDS )
